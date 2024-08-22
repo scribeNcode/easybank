d4346730abe85f05757d1f2655291b98644eb8e0
scribeNcode <deyeye99@gmail.com>
2024-08-22 12:28:10 +0100

    add text to why choose easybank and edit layout  section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,18 @@ export default {
       colors:{
         primary:{
           'darkBlue' : "hsl(233, 26%, 24%)",
-          limeGreen: "hsl(136, 65%, 51%)",
-          brightCyan: "hsl(192, 70%, 51%)"
+          'limeGreen': "hsl(136, 65%, 51%)",
+          'brightCyan': "hsl(192, 70%, 51%)"
         },
         secondary:{
-          100 : "#E2E2D5",
-          200 : "#888883",
+          'grayishBlue' : "hsl(233, 8%, 62%)",
+          'LgrayishBlue' : "hsl(220, 16%, 96%)",
+          'VLgrayishBlue' : "hsl(0, 0%, 98%)",
+          'white' : "hsl(0, 0%, 100%)"
         }
+      },
+      fontFamily:{
+        'public-sans':['Public Sans', 'sans-serif'],
       },
       screens:{
         'xs': '320px',
